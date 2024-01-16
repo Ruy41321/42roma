f0b097fcee68369c03f2cf78052b6c97b0a61ee0
Luigi Pennisi <lpennisi@student.42roma.it>
2024-01-16 19:19:17 +0100

    libft done

--- 42cursus/libft/ft_calloc.c
+++ 42cursus/libft/ft_calloc.c
@@ -2,13 +2,11 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	size_t	i;
 	void	*str;
 
 	str = malloc(size * nmemb);
 	if (!str)
 		return (NULL);
-	i = 0;
 	ft_bzero(str, size * nmemb);
 	return (str);
 }
--- 42cursus/libft/ft_putnbr_fd.c
+++ 42cursus/libft/ft_putnbr_fd.c
@@ -2,7 +2,7 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char *s;
+	char	*s;
 
 	s = ft_itoa(n);
 	ft_putstr_fd(s, fd);
